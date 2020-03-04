9b176948054942778b47d37d7995010b0e78d0e4
GarridoY <dgmn17@student.aau.dk>
2020-03-04 12:57:14 +0100

    Now works with Python 3
    Python 3 wants the string to be a bytes-like object to make the check if it is in "out".

--- robocupclient.py
+++ robocupclient.py
@@ -12,20 +12,20 @@ class RunServerMonitor:
         p = subprocess.Popen(unixDetection, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         out = p.communicate()[0]
 
-        if 'Mint' in out:
+        if 'Mint'.encode() in out:
             mintCommandrm = 'mate-terminal -e "rcssmonitor"'
             mintCommandrs = 'mate-terminal -e "rcssserver"'
             mintRunTerm = subprocess.Popen(mintCommandrm, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             mintRunTerm = subprocess.Popen(mintCommandrs, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-        if 'Ubuntu' in out:
+        if 'Ubuntu'.encode() in out:
             # otherwise gnome-terminal is the issued terminal for ubuntu 
             ubuntuCommandrm = 'mate-terminal -e "rcssmonitor"'            
             ubuntuCommandrs = 'mate-terminal -e "rcssserver"' 
             ubuntuRunTerm = subprocess.Popen(ubuntuCommandrm, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             ubuntuRunTerm = subprocess.Popen(ubuntuCommandrs, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-        if 'Darwin' in out:
+        if 'Darwin'.encode() in out:
             macOSCommandrs = """ osascript -e 'tell app "Terminal" to do script "rcssserver"' """
             macOSCommandrm = """ osascript -e 'tell app "Terminal" to do script "rcssmonitor"' """
             macOStermrs = subprocess.Popen(macOSCommandrs, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 

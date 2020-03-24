39fbcaafa428dd0fffbda835769f6ed932b282df
GarridoY <dgmn17@student.aau.dk>
2020-03-24 11:25:12 +0100

    rcg file parser v0.1
    Currently working on the flags on the field

--- src/test/parseTesting.py
+++ src/test/parseTesting.py
@@ -1,12 +1,33 @@
 import unittest
+from pyparsing import Word, nums, alphas, ParseException, alphanums
+
+
+integer  = Word(nums)
+var = Word(alphanums)
+arithOp  = Word("+-*/", max=1)
+
+expr = var + "=" + integer + arithOp + integer
+equation = expr
 
 
 class ParseTesting(unittest.TestCase):
-    def test_starting_out(self):
-        self.assertEqual(1, 1)
-
     def test_01(self):
-        self.assertEqual(10, 10)
+        test = "aa = 10 + 10"
+        equation.parseString(test)
+
+    def test_02(self):
+        test = "a = 10 + a"
+        with self.assertRaises(ParseException):
+            equation.parseString(test)
+    
+    def test_03(self):
+        test = "a = a + "
+        with self.assertRaises(ParseException):
+            equation.parseString(test)
+    
+    def test_04(self):
+        test = "a = 1000 + 50"
+        equation.parseString(test)
 
 
 if __name__ == "__main__":

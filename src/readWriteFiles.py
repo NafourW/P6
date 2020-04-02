a6f3824ab06bb6188fa6bcfb25a2b0a43f962ee8
Unknown <rwu17@student.aau.dk>
2020-04-02 13:45:19 +0200

    rcl & rcg file parser v1.0
    Combined commit and both parser should work

--- src/readWriteFiles.py
+++ src/readWriteFiles.py
@@ -12,35 +12,54 @@ class ReadWriteLogFiles:
         
 
     def readLogFileRCG(self):
-        #writeFile = open('logs/gameLog.rcg', 'w') # File to write to "new log"
-        """
-        with open('logs/incomplete.rcg', 'r') as fd: # Readom from this file for realtime logging
-            while True:
-                logData = fd.readline()
-                if logData:
-                    pass
-                    #writeFile.write(logData)
-                    #print(logData)
-        """
-        pass
+        #writeFile = open('logs/gameLog.rcl', 'w') # File to write to "new log"
+        with open('logfiles/incomplete.rcg', 'r') as fd: # Readom from this file for realtime logging
+            counter = 0
+            line = fd.readline()
+            rcgParser = rcgParsing()
+            while line:
+                counter += 1
+                try:
+                    rcgParser.strParsing(logData)
+                    #self.rcl_parsed_strings.append(logData)
+                except ParseException as e:
+                    print(e)
+                    break
+                
+                line = fd.readline()
+
+            print("Lines parsed" + str(counter))
+            error_line = line if line else "No errors while parsing .rcg file"
+            print(error_line)
+
+
 
     def readLogFileRCL(self):
         #writeFile = open('logs/gameLog.rcl', 'w') # File to write to "new log"
         with open('logfiles/incomplete.rcl', 'r') as fd: # Readom from this file for realtime logging
-            while True:
-                logData = fd.readline()
-                if logData:
-
-                    rclParser = rclParsing()
+            counter = 0
+            line = fd.readline()
+            rclParser = rclParsing()
+            while line:
+                counter += 1
+                try:
                     rclParser.strParsing(logData)
-                    self.rcl_parsed_strings.append(logData)
+                    #self.rcl_parsed_strings.append(logData)
+                except ParseException as e:
+                    print(e)
+                    break
+                
+                line = fd.readline()
 
-                    if 'time_over' in logData:
-                        print('BABY HIT ME ONE MORE TIME <3 ')
-                        break
+            print("Lines parsed" + str(counter))
+            error_line = line if line else "No errors while parsing .rcl file"
+            print(error_line)
+                    
+
+
     
     def get_rcl_parsed_strings(self):
-        return self.rcl_parsed_strings;
+        return self.rcl_parsed_strings
 
     def clear_parsed_strings(self):
         self.rcl_parsed_strings = []
